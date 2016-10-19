ea6957b33796a985da90fe8434036f894793314e
Chuck <cleone@campuslabs.con>
2016-10-19 16:36:15 -0400

    header and nav cleanup

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,8 @@
 import React from 'react';
 
 const Header = () => (
-       <header className="container-fluid top-bar">
-            <div className="row">
-            <a href="/" role="link"></a>
-            <main-menu className="ng-isolate-scope">
-            <div id="main-menu-wrapper">
-                <div id="nav-toggle-button">
-                    <div className="menu-bar"></div>
-                    <div className="menu-bar"></div>
-                    <div className="menu-bar"></div>
-                </div>
-                <nav id="main-menu" className="top-bar__nav">
-                    <ul>
-                        <li><a href="../">Home</a></li>
-                        <li><a href="../#/interaction">Interaction Patterns</a></li>
-                        <li><a href="/bootstrap" className="bootstrapGuide">Core UI (Bootstrap)</a></li>
-                        <li><a href="https://commons.campuslabs.today/CoreWeb/" target="_blank">Core UI (Legacy)</a></li>
-                        <li><a href="../#/brand">Product Design</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </main-menu>
-            </div>
-            </header>
+       <div>
+        <h1>Header</h1>
+       </div>
 )
 export default Header;
