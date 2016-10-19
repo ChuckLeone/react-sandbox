6e11e082bf68c8ef32825aa88422187130946ce3
Chuck <cleone@campuslabs.con>
2016-10-19 16:18:52 -0400

    fixed broken link to reducers folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
-import todoApp from './reducers/index'
+import todoApp from './sources/reducers/index'
 import App from './components/App';
 
 let store = createStore(todoApp)
